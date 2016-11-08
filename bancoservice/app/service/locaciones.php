0f5dfb71f79f2ee841b5363cd80a3311b9063fdc
YanCarlos <yancarlosmarinosorio@gmail.com>
2016-11-08 17:00:09 -0500

    Locaciones

--- bancoservice/app/service/locaciones.php
+++ bancoservice/app/service/locaciones.php
@@ -5,39 +5,33 @@
 
 /* Metodo que retorna las ciudades de un determinado departamento*/
 $app->get("/ciudades/:departamento",function($departamento) use($app){
-	try{
-		$connection= getConnection();
-		$dbf=$connection->prepare("select * from ciudades where departamento=" . $departamento);
-		$dbf->execute();
-		$resultado= $dbf->fetchAll();
-		$connection=null;
-
-        
-		$app->response->headers->set("Content-type","application/json");
-		$app->response->status(200);
-		print_r($resultado);
-	}catch(PDOException $e){
-		echo "Error: " . $e->getMessage();
-
-	}
+	$connection= getConnection();
+        $sth = $connection->prepare("select * from ciudades where departamento=" . $departamento );
+        $sth->execute();
+        $resultado = $sth->fetchAll();
+        $connection=null;
+        if ($resultado!=false) {
+            $resultado = array('respuesta' => true, 'resultado' => $resultado  );
+            $app->withJSON($resultado,200);
+        }else{
+            $resultado = array('respuesta' => false, 'mensaje' => 'No hay ciudades en la base de datos para el departamento ' . $departamento);
+            $app->withJSON($resultado,400);
+        }	
 });
 
 
 /* Metodo que retorna el departamento de una determinada ciudad*/
-$app->get("/departamento/:ciudad",function($ciudad) use($app){
-	try{
-		$connection= getConnection();
-		$dbf=$connection->prepare("select d.id,d.nombre from departamentos as d join ciudades as c on d.id=c.departamento where c.id=" . $ciudad);
-		$dbf->execute();
-		$resultado= $dbf->fetchAll();
-		$connection=null;
-
-        
-		$app->response->headers->set("Content-type","application/json");
-		$app->response->status(200);
-		print_r($resultado);
-	}catch(PDOException $e){
-		echo "Error: " . $e->getMessage();
-
-	}
+$app->get("/departamentos",function() use($app){
+	$connection= getConnection();
+        $sth = $connection->prepare("select * from departamentos");
+        $sth->execute();
+        $resultado = $sth->fetchAll();
+        $connection=null;
+        if ($resultado!=false) {
+            $resultado = array('respuesta' => true, 'resultado' => $resultado  );
+            $app->withJSON($resultado,200);
+        }else{
+            $resultado = array('respuesta' => false, 'mensaje' => 'No hay departamentos en la base de datos.');
+            $app->withJSON($resultado,400);
+        }	
 });
